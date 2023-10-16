f5d0c2873c65ad2ea7f5f5f81d334281f20a095f
Vincent Tencé <vtence@gmail.com>
2023-10-16 00:18:49 -0400

    fix(gutter): click handlers work again

--- lua/modules/ui/gutter.lua
+++ lua/modules/ui/gutter.lua
@@ -14,11 +14,11 @@ function M.config()
     -- configuration goes here, for example:
     relculright = true,
     segments = {
-      { text = { builtin.foldfunc }, click = "v.lua:ScFa" },
-      { sign = { name = { "Diagnostic" }, maxwidth = 1, colwidth = 1, auto = true }, click = "v.lua:ScSa" },
-      { sign = { name = { "Dap*" }, maxwidth = 1, colwidth = 1, auto = true }, click = "v.lua:ScSa" },
-      { text = { builtin.lnumfunc, " " }, click = "v.lua:ScLa" },
-      { sign = { namespace = { "gitsign" }, maxwidth = 1, colwidth = 2, auto = true }, click = "v.lua:ScSa" },
+      { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+      { sign = { name = { "Diagnostic" }, maxwidth = 1, colwidth = 1, auto = true }, click = "v:lua.ScSa" },
+      { sign = { name = { "Dap*" }, maxwidth = 1, colwidth = 1, auto = true }, click = "v:lua.ScSa" },
+      { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+      { sign = { namespace = { "gitsign" }, maxwidth = 1, colwidth = 2, auto = true }, click = "v:lua.ScSa" },
     }
   }
 end
