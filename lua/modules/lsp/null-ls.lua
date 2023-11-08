083920eeeb3dabc92ab3dd410b869d388ad025c8
Vincent Tencé <vtence@tower-research.com>
2023-11-08 07:11:29 -0500

    feat(lsp): manage null-ls using mason-null-ls

--- lua/modules/lsp/null-ls.lua
+++ lua/modules/lsp/null-ls.lua
@@ -5,34 +5,49 @@
 ]]
 
 local M = {
-  'jose-elias-alvarez/null-ls.nvim',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-  },
-  event = 'BufReadPre'
+    'jay-babu/mason-null-ls.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+        'williamboman/mason.nvim',
+        'jose-elias-alvarez/null-ls.nvim',
+        'nvim-lua/plenary.nvim',
+    },
 }
 
 function M.config()
-  local null_ls = require('null-ls')
-  local builtins = null_ls.builtins
+    local mason = require('mason-null-ls')
+    local null_ls = require('null-ls')
 
-  null_ls.setup {
-    sources = {
-      builtins.formatting.stylua,
-      builtins.formatting.prettier.with {
-        filetypes = {
-          'yaml',
-          'markdown',
+    mason.setup {
+        ensure_installed = { 'stylua', 'jq', 'black', 'prettier', 'flake8' },
+        handlers = {
+            function() end,                 -- disables automatic setup of all null-ls sources
+            stylua = function(source_name, methods)
+                null_ls.register(null_ls.builtins.formatting.stylua)
+            end,
+            shfmt = function(source_name, methods)
+                -- custom logic
+                mason.default_setup(source_name, methods)                                                                           -- to maintain default behavior
+            end,
         },
-        extra_filetypes = { 'toml' },
-        extra_args = { '--no-semi', '--single-quote', '--jsx-single-quote' }
-      },
-      builtins.formatting.black.with { extra_args = { '--fast' } },
-      builtins.completion.spell,
-      builtins.diagnostics.markdownlint,
-      --builtins.code_actions.gitsigns, -- we don't want blame line code action on every line of file
-    },
-  }
+    }
+
+    null_ls.setup {
+        sources = {
+            null_ls.builtins.formatting.prettier.with {
+                filetypes = {
+                    'yaml',
+                    'markdown',
+                },
+                extra_filetypes = { 'toml' },
+                extra_args = { '--no-semi', '--single-quote', '--jsx-single-quote' }
+            },
+            null_ls.builtins.formatting.black.with { extra_args = { '--fast' } },
+            null_ls.builtins.completion.spell,
+            null_ls.builtins.diagnostics.markdownlint,
+            --builtins.code_actions.gitsigns, -- we don't want blame line code action on every line of file
+        }
+    }
 end
 
 return M
