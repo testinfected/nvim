ee8b38061df1924b4cfa9ccb9f31b0eab30dd732
Vincent Tencé <vtence@tower-research.com>
2023-12-13 09:33:45 -0500

    feat(editor): use nvim version of beacon plugin

--- lua/modules/editor/cursor.lua
+++ lua/modules/editor/cursor.lua
@@ -1,9 +1,26 @@
 --[[
   Description: Flash cursor when it jumps some distance or moves between windows
-  See: https://github.com/DanilaMihailov/beacon.nvim
+  See: https://github.com/rainbowhxch/beacon.nvim
 ]]
 
-return {
-  'danilamihailov/beacon.nvim',
-  event = 'BufEnter'
+local M = {
+	'rainbowhxch/beacon.nvim',
+	event = 'VeryLazy',
 }
+
+function M.config()
+	require('beacon').setup({
+		enable = true,
+		size = 40,
+		fade = true,
+		minimal_jump = 10,
+		show_jumps = true,
+		focus_gained = false,
+		shrink = true,
+		timeout = 500,
+		ignore_buffers = {},
+		ignore_filetypes = {},
+	})
+end
+
+return M
