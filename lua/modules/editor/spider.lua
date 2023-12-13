9f120cc33f8f606fe5ab62ff4c05ed4a6afd57fa
Vincent Tencé <vtence@tower-research.com>
2023-12-13 09:34:54 -0500

    feat(editor): add spider movements plugin

---
+++ lua/modules/editor/spider.lua
@@ -0,0 +1,19 @@
+local M = {
+	"chrisgrieser/nvim-spider",
+	keys = {
+		{ "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" }, desc = "Move to next subword" },
+		{ "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" }, desc = "Move to end of subword" },
+		{ "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" }, desc = "Move to beginning of subword" },
+		{ "ge", "<cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "o", "x" }, desc = "Move to end of previous subword" },
+	},
+}
+
+function M.config()
+	-- default values
+	require("spider").setup({
+		skipInsignificantPunctuation = true,
+		subwordMovement = true,
+	})
+end
+
+return M
