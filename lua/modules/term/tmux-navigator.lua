e6253dba244b360235a882453464467bd0968ee0
Vincent Tencé <vtence@tower-research.com>
2023-12-13 09:38:55 -0500

    feat(tmux): change for nvim specific plugin

--- lua/modules/term/tmux-navigator.lua
+++ lua/modules/term/tmux-navigator.lua
@@ -1,13 +1,22 @@
 --[[
   Description: Seamless navigation between tmux panes and vim splits
-  See: https://github.com/christoomey/vim-tmux-navigator
+  See: https://github.com/alexghergh/nvim-tmux-navigation
 ]]
 
-return {
-  'christoomey/vim-tmux-navigator',
-  event = 'VeryLazy',
-  init = function()
-    -- Disable tmux navigator when zooming the Vim pane
-    vim.g.tmux_navigator_disable_when_zoomed = 1
-  end
+local M = {
+	"alexghergh/nvim-tmux-navigation",
+    lazy = false,
 }
+
+function M.config()
+    local tmux = require('nvim-tmux-navigation')
+	local map = vim.keymap.set
+	map("n", "<C-\\><C-j>", tmux.NvimTmuxNavigateDown, { desc = "Navigate down" })
+	map("n", "<C-\\><C-k>", tmux.NvimTmuxNavigateUp, { desc = "Navigate up" })
+	map("n", "<C-\\><C-l>", tmux.NvimTmuxNavigateRight, { desc = "Navigate right" })
+	map("n", "<C-\\><C-h>", tmux.NvimTmuxNavigateLeft, { desc = "Navigate left" })
+	map("n", "<C-\\><C-\\", tmux.NvimTmuxNavigateLastActive, { desc = "Navigate last" })
+	map("n", "<C-\\><C-Space>", tmux.NvimTmuxNavigateNext, { desc = "Navigate next" })
+end
+
+return M
