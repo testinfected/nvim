90d62bcebf09ebe6e168c7d46af5908baca63798
Vincent Tencé <vtence@tower-research.com>
2024-03-26 15:14:57 -0400

    feat(editor): replace multi-cursor plugin
    This one behaves more like VSCode/IntelliJ
    multiple cursors.

--- lua/modules/editor/multi-cursors.lua
+++ lua/modules/editor/multi-cursors.lua
@@ -1,15 +1,60 @@
 --[[
-  Description: Multiple cursors for vim/neovim
-  See: https://github.com/mg979/vim-visual-multi
+  Description: Multiple cursors for neovim
+  See: https://github.com/brenton-leighton/multiple-cursors.nvim
 ]]
-
 local M = {
-    'mg979/vim-visual-multi',
-    event = 'BufEnter'
+
+	"brenton-leighton/multiple-cursors.nvim",
+	version = "*", -- Use the latest tagged version
+	keys = {
+		{ "<C-Down>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "i" } },
+		{ "<C-j>", "<Cmd>MultipleCursorsAddDown<CR>" },
+		{ "<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "i" } },
+		{ "<C-k>", "<Cmd>MultipleCursorsAddUp<CR>" },
+		{ "<C-LeftMouse>", "<Cmd>MultipleCursorsMouseAddDelete<CR>", mode = { "n", "i" } },
+		{ "<Leader>a", "<Cmd>MultipleCursorsAddMatches<CR>", mode = { "n", "x" } },
+		{ "<Leader>A", "<Cmd>MultipleCursorsAddMatchesV<CR>", mode = { "n", "x" } },
+	},
 }
 
 function M.config()
-    vim.g.VM_leader = "\\"
+	require("multiple-cursors").setup({
+
+		custom_key_maps = {
+			-- w
+			{
+				{ "n", "x" }, "w",
+				function(_, count)
+					if count ~= 0 and vim.api.nvim_get_mode().mode == "n" then
+						vim.cmd("normal! " .. count)
+					end
+					require("spider").motion("w")
+				end,
+			},
+
+			-- e
+			{
+				{ "n", "x" }, "e",
+				function(_, count)
+					if count ~= 0 and vim.api.nvim_get_mode().mode == "n" then
+						vim.cmd("normal! " .. count)
+					end
+					require("spider").motion("e")
+				end,
+			},
+
+			-- b
+			{
+				{ "n", "x" }, "b",
+				function(_, count)
+					if count ~= 0 and vim.api.nvim_get_mode().mode == "n" then
+						vim.cmd("normal! " .. count)
+					end
+					require("spider").motion("b")
+				end,
+			},
+		},
+	})
 end
 
 return M
