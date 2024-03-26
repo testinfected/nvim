dd77447b4a0a0909653ac2968146f6895cb14722
Vincent Tencé <vtence@tower-research.com>
2024-03-26 15:16:42 -0400

    feat(term): pop terminal at the bottom
    Replace floating terminal with less
    intrusive bottom attached terminal.

--- lua/modules/term/toggleterm.lua
+++ lua/modules/term/toggleterm.lua
@@ -3,35 +3,43 @@
   See: https://github.com/akinsho/toggleterm.nvim
 ]]
 
-
 local M = {
-    'akinsho/toggleterm.nvim',
-    event = 'VeryLazy',
+	"akinsho/toggleterm.nvim",
+	event = "VeryLazy",
 }
 
 function M.config()
-    local toggleterm = require('toggleterm')
-    toggleterm.setup {
-        open_mapping = [[<leader>\]],
-        insert_mappings = false,
-        start_in_insert = true,
-        direction = 'float',
-        shade_terminals = true,
-        float_opts = {
-            border = 'curved'
-        },
-        on_open = function()
-            -- Prevent infinite calls from freezing neovim.
-            -- Only set these options specific to this terminal buffer.
-            vim.api.nvim_set_option_value("foldmethod", "manual", { scope = "local" })
-            vim.api.nvim_set_option_value("foldexpr", "0", { scope = "local" })
-        end,
-    }
+	local toggleterm = require("toggleterm")
+	toggleterm.setup({
+		open_mapping = [[<leader>\]],
+		insert_mappings = false,
+		start_in_insert = true,
+		direction = "horizontal", -- float|vertical|horizontal|tab
+		shade_terminals = true,
+		float_opts = {
+			border = "curved",
+		},
+		on_open = function()
+			-- Prevent infinite calls from freezing neovim.
+			-- Only set these options specific to this terminal buffer.
+			vim.api.nvim_set_option_value("foldmethod", "manual", { scope = "local" })
+			vim.api.nvim_set_option_value("foldexpr", "0", { scope = "local" })
+		end,
+		winbar = {
+			enabled = true,
+		},
+	})
 
-    -- Keymaps
-    local Terminal = require('toggleterm.terminal').Terminal
-    local lazygit = Terminal:new { cmd = 'lazygit', hidden = true }
-    vim.keymap.set('n', '<leader>gc', function() lazygit:toggle() end, { desc = "Commit" })
+	-- Keymaps
+	local Terminal = require("toggleterm.terminal").Terminal
+	local lazygit = Terminal:new({
+		cmd = "lazygit",
+		hidden = true,
+		direction = "float",
+	})
+	vim.keymap.set("n", "<leader>gc", function()
+		lazygit:toggle()
+	end, { desc = "Commit" })
 end
 
 return M
