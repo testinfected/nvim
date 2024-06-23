49fe70c39c14e4eb6dcb8d071092c57db26ba77b
testinfected <vtence@gmail.com>
2024-06-23 12:14:27 -0400

    chores(keymaps): add missing keys descriptions
    Add descriptions for missing multi-cursors
    keybindings.

--- lua/modules/editor/multi-cursors.lua
+++ lua/modules/editor/multi-cursors.lua
@@ -5,15 +5,72 @@
 local M = {
 
 	"brenton-leighton/multiple-cursors.nvim",
-	version = "*", -- Use the latest tagged version
 	keys = {
-		{ "<C-Down>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "i" } },
-		{ "<C-j>", "<Cmd>MultipleCursorsAddDown<CR>" },
-		{ "<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "i" } },
-		{ "<C-k>", "<Cmd>MultipleCursorsAddUp<CR>" },
-		{ "<C-LeftMouse>", "<Cmd>MultipleCursorsMouseAddDelete<CR>", mode = { "n", "i" } },
-		{ "<Leader>a", "<Cmd>MultipleCursorsAddMatches<CR>", mode = { "n", "x" } },
-		{ "<Leader>A", "<Cmd>MultipleCursorsAddMatchesV<CR>", mode = { "n", "x" } },
+		{
+			"<C-j>",
+			"<Cmd>MultipleCursorsAddDown<CR>",
+			mode = { "n", "x" },
+			desc = "Add cursor and move down",
+		},
+		{
+			"<C-k>",
+			"<Cmd>MultipleCursorsAddUp<CR>",
+			mode = { "n", "x" },
+			desc = "Add cursor and move up",
+		},
+
+		{
+			"<C-Up>",
+			"<Cmd>MultipleCursorsAddUp<CR>",
+			mode = { "n", "i", "x" },
+			desc = "Add cursor and move up",
+		},
+		{
+			"<C-Down>",
+			"<Cmd>MultipleCursorsAddDown<CR>",
+			mode = { "n", "i", "x" },
+			desc = "Add cursor and move down",
+		},
+
+		{
+			"<C-LeftMouse>",
+			"<Cmd>MultipleCursorsMouseAddDelete<CR>",
+			mode = { "n", "i" },
+			desc = "Add or remove cursor",
+		},
+
+		{
+			"<Leader>a",
+			"<Cmd>MultipleCursorsAddMatches<CR>",
+			mode = { "n", "x" },
+			desc = "Add cursors to nt word",
+		},
+		{
+			"<Leader>A",
+			"<Cmd>MultipleCursorsAddMatchesV<CR>",
+			mode = { "n", "x" },
+			desc = "Add cursors to current word in previous area",
+		},
+
+		{
+			"<Leader>d",
+			"<Cmd>MultipleCursorsAddJumpNextMatch<CR>",
+			mode = { "n", "x" },
+			desc = "Add cursor and jump to next cword",
+		},
+		{
+			"<Leader>D",
+			"<Cmd>MultipleCursorsJumpNextMatch<CR>",
+			mode = { "n", "x" },
+			desc = "Jump to next cword",
+		},
+
+		{
+			"<Leader>l",
+			"<Cmd>MultipleCursorsLock<CR>",
+			mode = { "n", "x" },
+			desc = "Lock virtual cursors",
+		},
 	},
 }
 
--- lua/modules/ui/which-key.lua
+++ lua/modules/ui/which-key.lua
@@ -5,17 +5,17 @@
 ]]
 
 local M = {
-    'folke/which-key.nvim',
-    event = 'VeryLazy'
+    "folke/which-key.nvim",
+    event = "VeryLazy",
 }
 
 function M.config()
-    local wk = require('which-key')
-    local icons = require('config.icons')
-    wk.setup {
+    local wk = require("which-key")
+    local icons = require("config.icons")
+    wk.setup({
         window = {
-            border = 'rounded', -- none, single, double, shadow
-            align = 'center',
+            border = "rounded", -- none, single, double, shadow
+            align = "center",
             margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
             padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
             winblend = 0,
@@ -23,20 +23,20 @@ function M.config()
         layout = {
             height = { min = 4, max = 25 }, -- min and max height of the columns
             width = { min = 20, max = 50 }, -- min and max width of the columns
-            spacing = 3, -- spacing between columns
-            align = "left", -- align columns left, center or right
+            spacing = 3,           -- spacing between columns
+            align = "left",        -- align columns left, center or right
         },
         icons = {
-			breadcrumb = icons.ui.Separator,
-        }
-    }
-
-    wk.register {
-        ['['] = {
-            name = "+Navigate (backward)"
+            breadcrumb = icons.ui.Separator,
         },
-        [']'] = {
-            name = "+Navigate (forward)"
+    })
+
+    wk.register({
+        ["["] = {
+            name = "+Navigate (backward)",
+        },
+        ["]"] = {
+            name = "+Navigate (forward)",
         },
         g = {
             name = "+Goto",
@@ -44,24 +44,27 @@ function M.config()
         z = {
             name = "+Fold",
         },
-        ['<c-w>'] = {
+        ["<c-w>"] = {
             name = "+Window",
         },
-        ['<leader>'] = {
+        ["<leader>"] = {
             name = "+User",
+            b = {
+                name = "+Buffer",
+            },
             c = {
                 name = "+Code",
                 d = {
                     name = "+Diagnostics",
-                    _ = 'which_key_ignore'
+                    _ = "which_key_ignore",
                 },
             },
             d = {
                 name = "+Debug",
                 b = {
                     name = "+Breakpoint",
-                    _ = 'which_key_ignore'
-                }
+                    _ = "which_key_ignore",
+                },
             },
             f = {
                 name = "+Find",
@@ -70,17 +73,31 @@ function M.config()
                 name = "+Git",
                 g = {
                     name = "+Show",
-                    _ = 'which_key_ignore'
+                    _ = "which_key_ignore",
                 },
                 t = {
                     name = "+Toggle",
-                    _ = 'which_key_ignore'
+                    _ = "which_key_ignore",
                 },
             },
             h = {
                 name = "+Help",
-                m = { function() vim.cmd [[WhichKey]] end, "Show mapping", },
-                k = { function() local input = vim.fn.input "WhichKey: " vim.cmd([[WhichKey ]] .. input) end, "Lookup key..." }
+                m = {
+                    function()
+                        vim.cmd([[WhichKey]])
+                    end,
+                    "Show mapping",
+                },
+                k = {
+                    function()
+                        vim.cmd([[Legendary]])
+                    end,
+                    "Lookup key...",
+                },
+            },
+            n = {
+                name = "+Navigate",
+                _ = "which_key_ignore",
             },
             o = {
                 name = "+Overseer",
@@ -97,12 +114,11 @@ function M.config()
             x = {
                 name = "+Trouble",
             },
-            ['\\'] = {
-                name = "+Multi cursors",
+            ["\\"] = {
+                name = "+Multi-cursors",
             },
-
-        }
-    }
+        },
+    })
 end
 
 return M
