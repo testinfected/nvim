e60a6d05cc06dfd2a5b9ca4a51b7f71283740587
Vincent Tencé <vtence@tower-research.com>
2024-06-29 21:20:47 -0400

    feat(go): fix range formatting and remove null-ls
    Remove null-ls and use navigator builtins.
    
    Fix an issue with Navigator range formatting
    code.
    
    Modify some lsp related icons to better
    suited ones.

--- lua/modules/lang/go.lua
+++ lua/modules/lang/go.lua
@@ -1,93 +1,71 @@
 --[[
-  File: go.lua
   Description: Feature-rich Go development plugin
   See: https://github.com/ray-x/go.nvim
 ]]
 
 local M = {
-	"ray-x/go.nvim",
-	dependencies = {
-		"ray-x/guihua.lua",
-		"neovim/nvim-lspconfig",
-	},
-	ft = { "go", "gomod" },
-	build = [[:lua require('go.install').update_all_sync()]], -- if you need to install/update all binaries
+    "ray-x/go.nvim",
+    dependencies = {
+        "ray-x/guihua.lua",
+        "neovim/nvim-lspconfig",
+    },
+    ft = { "go", "gomod" },
+    build = [[:lua require('go.install').update_all_sync()]], -- if you need to install/update all binaries
 }
 
 function M.config()
-	-- TODO:
-	-- [] configure test coverage
-	-- [] use lsp snippets from go.nvim
-	-- [] integrate null-ls
-	-- [] add buffer key mappings for test, etc.
+    require("go").setup({
+        icons = false,        -- We have already setup dap icons so don't want them overridden here
+        lsp_cfg = false,      -- Use mason, then apply gopls config after setup
+        lsp_codelens = false, -- use navigator
+        lsp_document_formatting = true,
+        lsp_keymaps = false,
+        dap_debug_keymap = false, -- I have set this up already with all my other nvim-dap setup
+        textobjects = false,      -- Already have textobjects set up so do not add a mapping for this
+        test_runner = "ginkgo",
+        trouble = true,
+        luasnip = true,
+        lsp_inlay_hints = {
+            enable = true,
+            -- whether to show variable name before type hints with the inlay hints or not
+            -- default: false
+            show_variable_name = true,
+            -- prefix for parameter hints
+            parameter_hints_prefix = "󰊕 ",
+            show_parameter_hints = true,
+            -- prefix for all the other hints (type, chaining)
+            other_hints_prefix = "=> ",
+            -- whether to align to the lenght of the longest line in the file
+            max_len_align = false,
+            -- padding from the left if max_len_align is true
+            max_len_align_padding = 1,
+            -- whether to align to the extreme right or not
+            right_align = false,
+            -- padding from the right if right_align is true
+            right_align_padding = 6,
+            -- The color of the hints
+            highlight = "Comment",
+        },
 
-	require("go").setup({
-		icons = false, -- We have already setup dap icons so don't want them overridden here
-		lsp_cfg = false, -- Use mason, then apply gopls config after setup
-		lsp_keymaps = false,
-		dap_debug_keymap = false, -- I have set this up already with all my other nvim-dap setup
-		textobjects = false, -- Already have textobjects set up so do not add a mapping for this
-		run_in_floaterm = true,
-		test_runner = "ginkgo",
-		trouble = true,
-		luasnip = true,
-		lsp_inlay_hints = {
-			enable = true,
-			-- whether to show variable name before type hints with the inlay hints or not
-			-- default: false
-			show_variable_name = true,
-			-- prefix for parameter hints
-			parameter_hints_prefix = "󰊕 ",
-			show_parameter_hints = true,
-			-- prefix for all the other hints (type, chaining)
-			other_hints_prefix = "=> ",
-			-- whether to align to the lenght of the longest line in the file
-			max_len_align = false,
-			-- padding from the left if max_len_align is true
-			max_len_align_padding = 1,
-			-- whether to align to the extreme right or not
-			right_align = false,
-			-- padding from the right if right_align is true
-			right_align_padding = 6,
-			-- The color of the hints
-			highlight = "Comment",
-		},
-	})
+        run_in_floaterm = true,
+        floaterm = {            -- position
+            posititon = 'auto', -- one of {`top`, `bottom`, `left`, `right`, `center`, `auto`}
+            width = 0.4,
+            height = 0.9,       -- height of float window if not auto
+            -- title_colors = {'#D8DEE9', '#5E81AC', '#88C0D0', '#EBCB8B', '#A3BE8C', '#B48EAD'}
+        },
+    })
 
-	-- Use go.nvim gopls config with lsp
-	require("go.lsp").setup()
+    -- Use go.nvim gopls config with lsp
+    require("go.lsp").setup()
 
-	local null_ls = require("null-ls")
-	local sources = {
-		null_ls.builtins.diagnostics.revive,
-		null_ls.builtins.formatting.golines.with({
-			extra_args = {
-				"--max-len=120",
-				"--base-formatter=gofumpt",
-			},
-		}),
-	}
-	-- for go.nvim
-	local gotest = require("go.null_ls").gotest()
-	local gotest_codeaction = require("go.null_ls").gotest_action()
-	local golangci_lint = require("go.null_ls").golangci_lint()
-	table.insert(sources, gotest)
-	table.insert(sources, golangci_lint)
-	table.insert(sources, gotest_codeaction)
-	null_ls.setup({ sources = sources, debounce = 1000, default_timeout = 5000 })
-
-	-- alternatively
-	--null_ls.register(gotest)
-
-	-- Run gofmt + goimports on save
-	-- local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
-	vim.api.nvim_create_autocmd("BufWritePre", {
-		pattern = "*.go",
-		callback = function()
-			require("go.format").goimports()
-		end,
-		group = format_sync_grp,
-	})
+    vim.api.nvim_create_autocmd('FileType', {
+        pattern = {'go'},
+        desc = "Go test key bindings",
+        callback = function()
+            vim.keymap.set("n", "<leader>tt", "[[<cmd>GoTestFile<cr>]]", { desc = "Run for current file" })
+        end
+    })
 end
 
 return M
--- lua/modules/lsp/navigator.lua
+++ lua/modules/lsp/navigator.lua
@@ -1,222 +1,240 @@
 local M = {
-	"ray-x/navigator.lua",
-	dependencies = {
-		{ "ray-x/lsp_signature.nvim" },
-		{ "ray-x/guihua.lua", run = "cd lua/fzy && make" },
-		{ "neovim/nvim-lspconfig" },
-	},
-	event = { "VeryLazy" },
+    "ray-x/navigator.lua",
+    dependencies = {
+        { "ray-x/lsp_signature.nvim" },
+        { "ray-x/guihua.lua",        run = "cd lua/fzy && make" },
+        { "neovim/nvim-lspconfig" },
+    },
+    event = { "VeryLazy" },
 }
 
 function M.config()
-	local icons = require("config.icons")
-	require("navigator").setup({
-		mason = true,
-		lsp_signature_help = true,
-		default_mapping = false,
-		icons = {
-			-- Code Action (gutter, floating window)
-			code_action_icon = icons.diagnostics.Action .. " ",
+    local icons = require("config.icons")
+    require("navigator").setup({
+        mason = true,
+        lsp_signature_help = true,
+        default_mapping = false,
+        icons = {
+            -- Code Action (gutter, floating window)
+            code_action_icon = icons.diagnostics.Action .. " ",
 
-			-- Code Lens (gutter, floating window)
-			code_lens_action_icon = "👓",
+            -- Code Lens (gutter, floating window)
+            code_lens_action_icon = "👓",
 
-			-- Diagnostics (gutter)
-			-- diagnostic_head = icons.diagnostics.Diagnostic, -- prefix for other diagnostic_* icons
-			diagnostic_head = "", -- prefix for other diagnostic_* icons
-			diagnostic_err = icons.diagnostics.Error,
-			diagnostic_warn = icons.diagnostics.Warning,
-			diagnostic_info = icons.diagnostics.Info,
-			diagnostic_hint = icons.diagnostics.Hint,
+            -- Diagnostics (gutter)
+            -- diagnostic_head = icons.diagnostics.Diagnostic, -- prefix for other diagnostic_* icons
+            diagnostic_head = "", -- prefix for other diagnostic_* icons
+            diagnostic_err = icons.diagnostics.Error,
+            diagnostic_warn = icons.diagnostics.Warning,
+            diagnostic_info = icons.diagnostics.Info,
+            diagnostic_hint = icons.diagnostics.Hint,
 
-			-- Diagnostics (floating window)
-			diagnostic_head_severity_1 = icons.diagnostics.Error,
-			diagnostic_head_severity_2 = icons.diagnostics.Warning,
-			diagnostic_head_severity_3 = icons.diagnostics.Info,
-			diagnostic_head_description = " ", -- suffix for severities
-			diagnostic_virtual_text = icons.diagnostics.Diagnostic, -- floating text preview (set to empty to disable)
-			diagnostic_file = icons.diagnostics.Diagnostic, -- icon in floating window, indicates the file contains diagnostics
+            -- Diagnostics (floating window)
+            diagnostic_head_severity_1 = icons.diagnostics.Error,
+            diagnostic_head_severity_2 = icons.diagnostics.Warning,
+            diagnostic_head_severity_3 = icons.diagnostics.Info,
+            diagnostic_head_description = " ",              -- suffix for severities
+            -- diagnostic_virtual_text = icons.diagnostics.Diagnostic, -- floating text preview (set to empty to disable)
+            diagnostic_virtual_text = "",                   -- floating text preview (set to empty to disable)
+            diagnostic_file = icons.diagnostics.Diagnostic, -- icon in floating window, indicates the file contains diagnostics
 
-			-- Values (floating window)
-			value_definition = "", -- identifier defined
-			value_changed = "", -- identifier modified
-			context_separator = " ", -- separator between text and value
+            -- Values (floating window)
+            value_definition = "", -- identifier defined
+            value_changed = "", -- identifier modified
+            context_separator = " ", -- separator between text and value
 
-			-- Formatting for Side Panel
-			side_panel = {
-				section_separator = "󰇜",
-				line_num_left = "",
-				line_num_right = "",
-				inner_node = "├○",
-				outer_node = "╰○",
-				bracket_left = "⟪",
-				bracket_right = "⟫",
-				tab = "󰌒",
-			},
-			fold = {
-				prefix = "⚡",
-				separator = "",
-			},
+            -- Formatting for Side Panel
+            side_panel = {
+                section_separator = "󰇜",
+                line_num_left = "",
+                line_num_right = "",
+                inner_node = "├○",
+                outer_node = "╰○",
+                bracket_left = "⟪",
+                bracket_right = "⟫",
+                tab = "󰌒",
+            },
+            fold = {
+                prefix = "⚡",
+                separator = "",
+            },
 
-			-- Treesitter
-			-- Note: many more node.type or kind may be available
-			match_kinds = {
-				var = icons.kind.Variable .. " ", -- variable --
-				const = icons.kind.Constant .. " ",
-				method = icons.kind.Method .. " ",
-				-- function is a keyword so wrap in ['key'] syntax
-				["function"] = icons.kind.Function .. " ",
-				parameter = icons.kind.Parameter .. " ",
-				parameters = icons.kind.Parameter .. " ",
-				require_parameter = icons.kind.Parameter .. " ",
-				associated = icons.kind.Reference .. " ", -- linked/related
-				namespace = icons.kind.Namespace .. " ",
-				type = icons.kind.Type .. " ",
-				field = icons.kind.Field .. " ",
-				module = icons.kind.Module .. " ",
-				flag = icons.kind.Event .. " ",
-			},
-			treesitter_defult = icons.lsp.Treesitter .. " ", -- default symbol when unknown node.type or kind
-			doc_symbols = "", -- document
-		},
-		keymaps = {
-			{
-				key = "gd",
-				func = require("navigator.definition").definition,
-				desc = "Definition",
-			},
-			{
-				key = "<leader>nd",
-				func = require("navigator.definition").definition,
-				desc = "Definition",
-			},
-			{
-				key = "gr",
-				func = require("navigator.reference").async_ref,
-				desc = "Reference",
-			},
-			{
-				key = "<leader>nr",
-				func = require("navigator.reference").async_ref,
-				desc = "Reference",
-			},
-			{
-				key = "<leader>np",
-				func = require("navigator.definition").definition_preview,
-				desc = "Definition Preview",
-			},
-			{
-				key = "<leader>nt",
-				func = vim.lsp.buf.type_definition,
-				desc = "Type Definition",
-			},
-			{
-				key = "<leader>nu",
-				func = require("navigator.definition").type_definition_preview,
-				desc = "Type Definition Preview",
-			},
-			{
-				key = "<leader>ni",
-				func = vim.lsp.buf.incoming_calls,
-				desc = "Incoming Calls",
-			},
-			{
-				key = "<leader>no",
-				func = vim.lsp.buf.outgoing_calls,
-				desc = "Outgoing Calls",
-			},
-			{
-				key = "<leader>nb",
-				func = vim.lsp.buf.implementation,
-				desc = "Implementation",
-			},
-			{
-				key = "<leader>nk",
-				func = require("navigator.dochighlight").hi_symbol,
-				desc = "Highlight Symbol",
-			},
-			{
-				key = "<leader>ns",
-				func = vim.lsp.buf.signature_help,
-				desc = "Signature",
-			},
-			{
-				key = "<leader>ca",
-				mode = "n",
-				func = require("navigator.codeAction").code_action,
-				desc = "Code Action",
-			},
-			{
-				key = "<leader>ca",
-				mode = "v",
-				func = require("navigator.codeAction").range_code_action,
-				desc = "Range Code Action",
-			},
-			{
-				key = "<leader>cl",
-				mode = "n",
-				func = require("navigator.codelens").run_action,
-				desc = "Code Lens Action",
-			},
-			{
-				key = "<leader>cf",
-				func = require("navigator.formatting").range_format,
-				mode = "n",
-				desc = "Range Format Operator",
-			},
-			{
-				key = "<leader>cs",
-				func = require("navigator.symbols").document_symbols,
-				desc = "Document Symbols",
-			},
-			{
-				key = "<leader>cw",
-				func = require("navigator.workspace").workspace_symbol_live,
-				desc = "Workspace Symbols",
-			},
-			{
-				key = "<leader>cr",
-				func = require("navigator.rename").rename,
-				desc = "Rename",
-			},
-			-- Diagnostics
-			{
-				key = "<leader>cdw",
-				func = require("navigator.diagnostics").show_diagnostics,
-				desc = "Workspace Diagnostics",
-			},
-			{
-				key = "<leader>cdb",
-				func = require("navigator.diagnostics").show_buf_diagnostics,
-				desc = "Buffer diagnostics",
-			},
-			{
-				key = "<leader>cdt",
-				func = require("navigator.diagnostics").toggle_diagnostics,
-				desc = "Toggle Diagnostics",
-			},
-			{
-				key = "]d",
-				func = require("navigator.diagnostics").goto_next,
-				desc = "Next Error",
-			},
-			{
-				key = "[d",
-				func = require("navigator.diagnostics").goto_prev,
-				desc = "Prev Error",
-			},
-			{
-				key = "]r",
-				func = require("navigator.treesitter").goto_next_usage,
-				desc = "Next Usage",
-			},
-			{
-				key = "[r",
-				func = require("navigator.treesitter").goto_previous_usage,
-				desc = "Previous Usage",
-			},
-		},
-	})
+            -- Treesitter
+            -- Note: many more node.type or kind may be available
+            match_kinds = {
+                var = icons.kind.Variable .. " ", -- variable --
+                const = icons.kind.Constant .. " ",
+                method = icons.kind.Method .. " ",
+                -- function is a keyword so wrap in ['key'] syntax
+                ["function"] = icons.kind.Function .. " ",
+                parameter = icons.kind.Parameter .. " ",
+                parameters = icons.kind.Parameter .. " ",
+                require_parameter = icons.kind.Parameter .. " ",
+                associated = icons.kind.Reference .. " ", -- linked/related
+                namespace = icons.kind.Namespace .. " ",
+                type = icons.kind.Type .. " ",
+                field = icons.kind.Field .. " ",
+                module = icons.kind.Module .. " ",
+                flag = icons.kind.Event .. " ",
+            },
+            treesitter_defult = icons.lsp.Treesitter .. " ", -- default symbol when unknown node.type or kind
+            doc_symbols = "", -- document
+        },
+        keymaps = {
+            {
+                key = "gd",
+                func = require("navigator.definition").definition,
+                desc = "Definition",
+            },
+            {
+                key = "<leader>nd",
+                func = require("navigator.definition").definition,
+                desc = "Definition",
+            },
+            {
+                key = "gr",
+                func = require("navigator.reference").async_ref,
+                desc = "Reference",
+            },
+            {
+                key = "<leader>nr",
+                func = require("navigator.reference").async_ref,
+                desc = "Reference",
+            },
+            {
+                key = "<leader>nt",
+                func = vim.lsp.buf.type_definition,
+                desc = "Type Definition",
+            },
+            {
+                key = "<leader>ni",
+                func = vim.lsp.buf.incoming_calls,
+                desc = "Incoming Calls",
+            },
+            {
+                key = "<leader>no",
+                func = vim.lsp.buf.outgoing_calls,
+                desc = "Outgoing Calls",
+            },
+            {
+                key = "<leader>ns",
+                func = require("navigator.symbols").document_symbols,
+                desc = "Document Symbols",
+            },
+            {
+                key = "<leader>nw",
+                func = require("navigator.workspace").workspace_symbol_live,
+                desc = "Workspace Symbols",
+            },
+            {
+                key = "<leader>nb",
+                func = vim.lsp.buf.implementation,
+                desc = "Implementation",
+            },
+            {
+                key = "<leader>ca",
+                mode = "n",
+                func = require("navigator.codeAction").code_action,
+                desc = "Action",
+            },
+            {
+                key = "<leader>ca",
+                mode = "v",
+                func = require("navigator.codeAction").range_code_action,
+                desc = "Ranged action",
+            },
+            {
+                key = "<leader>ch",
+                func = require("navigator.dochighlight").hi_symbol,
+                desc = "Highlight symbol",
+            },
+            {
+                key = "<leader>cl",
+                mode = "n",
+                func = require("navigator.codelens").run_action,
+                desc = "Lens",
+            },
+            {
+                key = "<leader>cf",
+                func = require("navigator.formatting").format_hdl,
+                mode = "n",
+                desc = "Format",
+            },
+            {
+                key = "<leader>cf",
+                -- require("navigator.formatting").range_format is currently broken
+                -- see https://github.com/ray-x/navigator.lua/issues/309
+                func = function()
+                    local old_func = vim.go.operatorfunc
+                    _G.op_func_formatting = function()
+                      vim.lsp.buf.format()
+                      vim.go.operatorfunc = old_func
+                      _G.op_func_formatting = nil
+                    end
+                    vim.go.operatorfunc = 'v:lua.op_func_formatting'
+                    vim.api.nvim_feedkeys('g@', 'n', false)
+                  end,
+                mode = "v",
+                desc = "Format selected",
+            },
+            {
+                key = "<leader>cd",
+                func = require("navigator.definition").definition_preview,
+                desc = "Preview definition",
+            },
+            {
+                key = "<leader>ct",
+                func = require("navigator.definition").type_definition_preview,
+                desc = "Preview type definition",
+            },
+            {
+                key = "<leader>cr",
+                func = require("navigator.rename").rename,
+                desc = "Rename",
+            },
+            {
+                key = "<leader>ns",
+                func = vim.lsp.buf.signature_help,
+                desc = "Signature",
+            },
+            -- Diagnostics
+            {
+                key = "<leader>cdw",
+                func = require("navigator.diagnostics").show_diagnostics,
+                desc = "Workspace",
+            },
+            {
+                key = "<leader>cdb",
+                func = require("navigator.diagnostics").show_buf_diagnostics,
+                desc = "Buffer",
+            },
+            {
+                key = "<leader>cdt",
+                func = require("navigator.diagnostics").toggle_diagnostics,
+                desc = "Toggle",
+            },
+            {
+                key = "]d",
+                func = require("navigator.diagnostics").goto_next,
+                desc = "Jump to next diagnostic",
+            },
+            {
+                key = "[d",
+                func = require("navigator.diagnostics").goto_prev,
+                desc = "Jump to previous diagnostic",
+            },
+            {
+                key = "]u",
+                func = require("navigator.treesitter").goto_next_usage,
+                desc = "Jumpt to next usage",
+            },
+            {
+                key = "[u",
+                func = require("navigator.treesitter").goto_previous_usage,
+                desc = "Jump to previous usage",
+            },
+        },
+    })
 end
 
 return M
