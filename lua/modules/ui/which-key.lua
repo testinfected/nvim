cd2dd7c812bf157f38ff9b79b4932e7d24a35995
Vincent Tencé <vtence@tower-research.com>
2024-10-11 15:17:21 -0400

    fix(which-key): update keymap to latest spec
    Update which key mappings to new API

--- lua/modules/ui/which-key.lua
+++ lua/modules/ui/which-key.lua
@@ -1,24 +1,30 @@
 --[[
-  File: which-key.lua
   Description: Displays a popup with possible key bindings
   See: https://github.com/folke/which-key.nvim
 ]]
 
 local M = {
     "folke/which-key.nvim",
+    dependencies = {
+        "echasnovski/mini.icons",
+    },
     event = "VeryLazy",
 }
 
 function M.config()
+    require('mini.icons').setup()
     local wk = require("which-key")
     local icons = require("config.icons")
     wk.setup({
-        window = {
-            border = "rounded",       -- none, single, double, shadow
-            align = "center",
-            margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]
-            padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-            winblend = 0,
+        preset = "modern",
+        win = {
+            border = "rounded",           -- none, single, double, shadow
+            title_pos = "center",
+            -- margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]
+            -- padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+            wo = {
+                winblend = 0,
+            },
         },
         layout = {
             height = { min = 4, max = 25 }, -- min and max height of the columns
@@ -31,94 +37,31 @@ function M.config()
         },
     })
 
-    wk.register({
-        ["["] = {
-            name = "+Jump back",
-        },
-        ["]"] = {
-            name = "+Jump forward",
-        },
-        g = {
-            name = "+Goto",
-        },
-        z = {
-            name = "+Fold",
-        },
-        ["<c-w>"] = {
-            name = "+Window",
-        },
-        ["<leader>"] = {
-            name = "+User",
-            b = {
-                name = "+Buffer",
-            },
-            c = {
-                name = "+Code",
-                d = {
-                    name = "+Diagnostics",
-                    _ = "which_key_ignore",
-                },
-            },
-            d = {
-                name = "+Debug",
-                b = {
-                    name = "+Breakpoint",
-                    _ = "which_key_ignore",
-                },
-            },
-            f = {
-                name = "+Find",
-            },
-            g = {
-                name = "+Git",
-                g = {
-                    name = "+Show",
-                    _ = "which_key_ignore",
-                },
-                t = {
-                    name = "+Toggle",
-                    _ = "which_key_ignore",
-                },
-            },
-            h = {
-                name = "+Help",
-                m = {
-                    function()
-                        vim.cmd([[WhichKey]])
-                    end,
-                    "Show mapping",
-                },
-                k = {
-                    function()
-                        vim.cmd([[Legendary]])
-                    end,
-                    "Lookup key...",
-                },
-            },
-            m = {
-                name = "+Multi-cursors",
-                _ = "which_key_ignore",
-            },
-            n = {
-                name = "+Navigate code",
-                _ = "which_key_ignore",
-            },
-            o = {
-                name = "+Overseer",
-            },
-            r = {
-                name = "+Replace",
-            },
-            w = {
-                name = "+Window",
-            },
-            t = {
-                name = "+Test",
-            },
-            x = {
-                name = "+Trouble",
-            },
-        },
+    wk.add({
+        { "[",          group = "Jump back", },
+        { "]",          group = "Jump forward", },
+        { "g",          group = "Goto" },
+        { "z",          group = "Fold" },
+        { "<c-w>",      group = "Window" },
+        { "<leader>b",  group = "Buffer" },
+        { "<leader>c",  group = "Code" },
+        { "<leader>cd", group = "Diagnostics" },
+        { "<leader>d",  group = "Debug" },
+        { "<leader>db", group = "Breakpoint" },
+        { "<leader>f",  group = "Find" },
+        { "<leader>g",  group = "Git" },
+        { "<leader>gg", group = "Show" },
+        { "<leader>gt", group = "Toggle" },
+        { "<leader>h",  group = "Help" },
+        { "<leader>hm", function() vim.cmd([[WhichKey]]) end,  desc = "Show mapping" },
+        { "<leader>hk", function() vim.cmd([[Legendary]]) end, desc = "Lookup key..." },
+        { "<leader>m",  group = "Multicursors" },
+        { "<leader>n",  group = "Navigate" },
+        { "<leader>o",  group = "Oversee" },
+        { "<leader>r",  group = "Replace" },
+        { "<leader>t",  group = "Test" },
+        { "<leader>w",  group = "Window" },
+        { "<leader>x",  group = "Trouble" },
     })
 end
 
