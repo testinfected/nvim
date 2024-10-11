b89233ce0e8f7ed90b16a33a3aca1b3a0e79b0f3
Vincent Tencé <vtence@tower-research.com>
2024-10-11 15:23:40 -0400

    fix(leap): avoid keymap conflicts

--- lua/modules/editor/leap.lua
+++ lua/modules/editor/leap.lua
@@ -9,26 +9,11 @@ local M = {
 }
 
 function M.config()
-    require("leap").add_default_mappings()
-
-    -- Avoid conflicts with vim-surround
-    vim.g["surround_no_mappings"] = 1
-    local map = vim.keymap.set
-    -- Just the defaults copied here.
-    map("n", "ds", "<Plug>Dsurround")
-    map("n", "cs", "<Plug>Csurround")
-    map("n", "cS", "<Plug>CSurround")
-    map("n", "ys", "<Plug>Ysurround")
-    map("n", "yS", "<Plug>YSurround")
-    map("n", "yss", "<Plug>Yssurround")
-    map("n", "ySs", "<Plug>YSsurround")
-    map("n", "ySS", "<Plug>YSsurround")
-
-    -- The conflicting ones. Note that `<Plug>(leap-cross-window)`
-    -- _does_ work in Visual mode, if jumping to the same buffer,
-    -- so in theory, `gs` could be useful for Leap too...
-    map("x", "gs", "<Plug>VSurround")
-    map("x", "gS", "<Plug>VgSurround")
+    -- Don't use leap in operator pending mode to avoid conflicts with nvim surround
+    map = vim.keymap.set
+    map({ 'n', 'x' }, 's', '<Plug>(leap-forward)')
+    map({ 'n', 'x' }, 'S', '<Plug>(leap-backward)')
+    map({ 'n', 'x' }, 'gs', '<Plug>(leap-from-window)')
 end
 
 return M
