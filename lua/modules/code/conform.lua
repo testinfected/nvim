27018a08e7968d3c38ce7eedea9cc8ca5f9d6e0f
Vincent Tencé <vtence@tower-research.com>
2025-01-10 11:58:20 -0500

    feat(rust): add rustecean plugin
    Add `rustecean` plugin for rust development
    and `conform` for formatting.

---
+++ lua/modules/code/conform.lua
@@ -0,0 +1,40 @@
+local M = {
+    "stevearc/conform.nvim",
+    opts = {},
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+        {
+            "<leader>bf",
+            function()
+                require("conform").format({ async = true })
+            end,
+            desc = "Format",
+        },
+    },
+    init = function()
+        -- If you want the formatexpr, here is the place to set it
+        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+}
+
+function M.config()
+    require("conform").setup({
+        formatters_by_ft = {
+            lua = { "stylua" },
+            -- Conform will run multiple formatters sequentially
+            python = { "isort", "black" },
+            -- You can customize some of the format options for the filetype (:help conform.format)
+            rust = { "rustfmt", lsp_format = "fallback" },
+            -- Conform will run the first available formatter
+            javascript = { "prettierd", "prettier", stop_after_first = true },
+        },
+        format_on_save = {
+            -- These options will be passed to conform.format()
+            timeout_ms = 500,
+            lsp_format = "fallback",
+        },
+    })
+end
+
+return M
