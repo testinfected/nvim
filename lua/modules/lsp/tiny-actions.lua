845db9abefbfbb6d9fba412b3fdad0a8561d7d70
Vincent Tencé <vtence@tower-research.com>
2025-09-29 14:14:27 -0400

    feat(code action): add tiny actions plugin

---
+++ lua/modules/lsp/tiny-actions.lua
@@ -0,0 +1,33 @@
+return {
+	"rachartier/tiny-code-action.nvim",
+	dependencies = {
+		{ "nvim-lua/plenary.nvim" },
+		{
+			"folke/snacks.nvim",
+		},
+	},
+	event = "LspAttach",
+	config = function()
+		local actions = require("tiny-code-action")
+		actions.setup({
+			backend = "difftastic", -- choice of vim, delta, difftastic and diffsofancy
+			picker = "snacks",
+			signs = {
+				quickfix = { "", { link = "DiagnosticWarning" } },
+				others = { "", { link = "DiagnosticWarning" } },
+				refactor = { "", { link = "DiagnosticInfo" } },
+				["refactor.move"] = { "󰪹", { link = "DiagnosticInfo" } },
+				["refactor.extract"] = { "", { link = "DiagnosticError" } },
+				["source.organizeImports"] = { "", { link = "DiagnosticWarning" } },
+				["source.fixAll"] = { "󰃢", { link = "DiagnosticError" } },
+				["source"] = { "", { link = "DiagnosticError" } },
+				["rename"] = { "󰑕", { link = "DiagnosticWarning" } },
+				["codeAction"] = { "", { link = "DiagnosticWarning" } },
+			},
+		})
+		-- Override default code action
+		vim.keymap.set({ "n", "x" }, "gra", function()
+			require("tiny-code-action").code_action()
+		end, { noremap = true, silent = true, desc = "Code actions" })
+	end,
+}
