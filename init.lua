df313e0c31392d5012c4f59eba9afc9c4c55b00f
Vincent Tencé <vtence@tower-research.com>
2025-10-10 17:46:41 -0400

    chores: use the module names

--- init.lua
+++ init.lua
@@ -8,16 +8,18 @@ require("config.keymaps")
 
 local modules = require("core.loader")
 modules.load({
-	"lsp",
-	"completion",
-	"debugger",
-	"lang",
-	"tools",
-	"tools.treesitter",
-	"editor",
 	"code",
+	"completion",
+	"debugging",
+	"editing",
+	"keybindings",
+	"lang",
+	"lsp",
+	"motion",
+	"syntax",
 	"term",
 	"ui",
+	"utility",
 })
 
 -- vim:tabstop=2 shiftwidth=2 expandtab syntax=lua foldmethod=marker foldlevelstart=0 foldlevel=0
